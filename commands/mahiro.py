586bf452c1a2f2939b3904f82ab13a4a07c6c51a
Reiko Dev <152267140+codebox4chan@users.noreply.github.com>
2024-01-26 10:55:55 +0800

    Update mahiro.py

--- commands/mahiro.py
+++ commands/mahiro.py
@@ -1,14 +1,14 @@
 def command(input_value, thread_userid=None):
     config = {
-        "name": "mahiro",
+        "name": "link",
         "version": "1.0.0",
-        "description": "Developer.",
-        "credits": "Mahiro chan",
+        "description": "Testing Only.",
+        "credits": "Kenneth Panio",
         "cooldown": "1"
     }
     if input_value == "__config__":
         return config
     elif input_value.startswith(config['name'] + ' ') or input_value == config['name']: 
-    	return {'messages': ["DEVELOPED BY MAHIRO CHAN"], 'image': ['commands/images/onimai-oniichan-wa-oshimai.gif']}
+    	return {'messages': ["Ugh!"], 'image': ['https://i.imgur.com/m7iPAc3.gif']}
     else:
         return {'messages': [f"Default option in {config['name']}: {config['description']}"]}
